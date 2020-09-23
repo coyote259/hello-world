e27349ffd33d380670d9b810f5f9bd1118bbd474
Eason Sun <eason.sun@ni.com>
2020-09-23 10:42:25 +0800

    Add Test.py

---
+++ Test.py
@@ -0,0 +1,13 @@
+name = input("please type your name:")
+print("hello,",name)
+
+# print absolute value of an integer:
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+print('''line1
+line2
+line3''')
